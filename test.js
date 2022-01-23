3ee1aa2a2bc8b4a85ef5a17a6f57b402321f7fa1
WinterSoldier16 <vsg8@hi.is>
2022-01-23 21:59:06 +0000

    npm, stylelint og eslint sett upp ásamt beinagrind

---
+++ test.js
@@ -0,0 +1,14 @@
+Globalize = require("globalize");
+Globalize.load(
+	require("cldr-data/supplemental/likelySubtags"),
+	require("cldr-data/main/de/numbers"),
+	require("cldr-data/supplemental/numberingSystems")
+);
+Globalize.locale("de");
+//Globalize.load(require("cldr-data").entireSupplemental());
+//Globalize.load(require("cldr-data").entireMainFor("en", "es"));
+console.log(Globalize.numberParser()("10.000,00"));
+console.log(Globalize.numberParser()("10.000,94"));
+console.log(Globalize.numberParser()("6.4e3"));
+console.log(Number("6.4e7"));
+console.log(Number("10.000,94"));
